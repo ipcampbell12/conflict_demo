9f23b5f7a798288926e460ca382c488258773bc7
ipcampbell12 <ipcampbell12@gmail.com>
2022-04-25 14:43:47 -0700

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,26 @@
-<<<<<<< HEAD
+<< << << < HEAD
+
+
 def hello_world(state):
     print(f'Hello World {state}!')
 
 
 def main():
     hello_world('New York')
-=======
-def hello_world(city):
-    print(f'Hello World! {city}')
+
+
+== == == =
+
+
+def hello_world(city, state):
+    print(f'Hello World! {city},{state}!')
 
 
 def main():
-    hello_world('NYC')
->>>>>>> ba0a9767f9f2392d2e292742f9d335cf84ac9d58
+    hello_world('NYC', 'New York')
+
+
+>>>>>> > ba0a9767f9f2392d2e292742f9d335cf84ac9d58
 
 
 main()
